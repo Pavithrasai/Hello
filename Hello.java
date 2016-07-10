364301d976b8339ed17a9248e6fea2c86f502a04
Pavithrasai <pavithrambe25@gmail.com>
2016-07-10 11:19:49 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Hell
+{
+public static void main(String[]args)
+{
+String s="Hello";
+for(int i=1;i<=5;i++)
+{
+System.out.println(s);
+}
+}
+}
